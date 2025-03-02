40abd6e90a403ed1c3920c91a9886b58d932d821
Himanshu Ojha <ojhaojhahimanshu@gmail.com>
2025-03-02 22:56:24 +0530

    Project 15

---
+++ P15-Heart/app.js
@@ -0,0 +1,18 @@
+const grayHeart= document.querySelector(".gray-heart");
+const redHeart= document.querySelector(".red-heart");
+
+
+grayHeart.addEventListener('click', ()=> {
+
+    redHeart.classList.add('animation');
+    grayHeart.classList.add('fill-color');
+    redHeart.style.visibility = "visible"; 
+
+})
+redHeart.addEventListener('click', ()=> {
+
+    redHeart.classList.remove('animation');
+    grayHeart.classList.remove('fill-color');
+    redHeart.style.visibility = "hidden";
+
+})
