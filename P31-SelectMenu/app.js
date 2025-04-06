8a5f4a31fb17aeccc55c9f66676c382589819fc5
Himanshu Ojha <ojhaojhahimanshu@gmail.com>
2025-04-06 12:49:17 +0530

    project 30

---
+++ P31-SelectMenu/app.js
@@ -0,0 +1,24 @@
+let selectField = document.getElementById("selectField");
+
+let selectText = document.getElementById("selectText");
+
+let list = document.getElementById("list");
+
+let arrwoIcon = document.getElementById("arrowIcon");
+let options = document.getElementsByClassName("options");
+
+selectField.addEventListener('click', () => {
+
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+
+});
+
+
+for (option of options) {
+    option.onclick = function () {
+        selectText.innerHTML = this.textContent;
+        arrowIcon.classList.toggle("rotate");
+    }
+}
+
