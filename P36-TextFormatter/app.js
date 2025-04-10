8c760452f5d70ded818b069efc76a88f0d09b996
Himanshu Ojha <ojhaojhahimanshu@gmail.com>
2025-04-10 13:38:51 +0530

    project 36

--- P36-TextFormatter/app.js
+++ P36-TextFormatter/app.js
@@ -4,7 +4,25 @@ const buttons = document.querySelectorAll("button");
 
 inputField.addEventListener(
   "keyup",
-  () => (outputField.innerHTML = inputField.value),
+  () => (outputField.innerHTML = inputField.value)
 );
 
-buto;
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("uppercase")) {
+      outputField.innerHTML = outputField.innerHTML.toUpperCase();
+    } else if (btn.classList.contains("capitalize")) {
+      outputField.innerHTML =
+        outputField.innerHTML.charAt(0).toUpperCase() +
+        outputField.innerHTML.slice(1).toLowerCase();
+    } else if (btn.classList.contains("lowercase")) {
+      outputField.innerHTML = outputField.innerHTML.toLowerCase();
+    } else if (btn.classList.contains("bold")) {
+      outputField.style.fontWeight = "900";
+    } else if (btn.classList.contains("italic")) {
+      outputField.style.fontStyle = "italic";
+    } else if (btn.classList.contains("underline")) {
+      outputField.style.textDecoration = "underline";
+    }
+  });
+});
